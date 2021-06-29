e1d87c3fbaa113242a309d11f18c05b9ba7d0e58
MarkD257 <mwd1@peoplepc.com>
2021-06-29 10:44:14 -0400

    Add files via upload

---
+++ GDCConsoleApp/WorkerProcess.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using GDCConsoleApp.Data;
+using GDCConsoleApp.Services;
+
+namespace GDCConsoleApp
+{
+    public class WorkerProcess
+    {
+        private readonly ICSVReader dataReader;
+
+
+        public IEnumerable<Player> players;
+
+        public List<Player> validPlayerEmails;
+        public List<Player> invalidPlayerEmails;
+
+        public WorkerProcess(ICSVReader dataReader)
+        {
+            this.dataReader = dataReader;
+        }
+
+        public void LoadPlayers(string fileName)
+        {
+            try
+            {
+                this.dataReader.filePath = fileName;
+                players = dataReader.GetPlayers();
+
+                validPlayerEmails = players.Where(p => RegexUtilities.IsValidEmail(p.EmailAddress) == true).ToList();
+                invalidPlayerEmails = players.Where(p => RegexUtilities.IsValidEmail(p.EmailAddress) == false).ToList();
+
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public string DataReaderType
+        {
+            get { return dataReader.GetType().ToString(); }
+        }
+    }
+}
