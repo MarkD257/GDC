b1fb4a307b9810a6f3adf64e81b45c4f5f6988fd
MarkD257 <mwd1@peoplepc.com>
2021-06-29 08:26:41 -0400

    Add files via upload

---
+++ GDCConsoleApp/Data/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GDCConsoleApp.Data
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FormatString { get; set; }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(FormatString))
+                FormatString = "{0} {1}";
+            return string.Format(FormatString, LastName, EmailAddress);
+        }
+    }
+}
---
+++ GDCConsoleApp/Models/PlayerModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+using GDCConsoleApp.Data;
+using GDCConsoleApp.Services;
+
+namespace GDCConsoleApp.Models
+{
+    public class PlayerViewModel
+    {
+        protected ICSVReader DataReader;
+
+        private IEnumerable<Player> _players;
+       
+        public IEnumerable<Player> Players
+        {
+            get { return _players; }
+            set
+            {
+                if (_players == value)
+                    return;
+                _players = value;
+                 }
+        }
+
+        public PlayerViewModel(ICSVReader dataReader)
+        {
+            DataReader = dataReader;
+        }
+
+        public void RefreshPlayers()
+        {
+            Players = DataReader.GetPlayers();
+        }
+        public void ClearPlayers()
+        {
+            Players = new List<Player>();
+        }
+
+        public string DataReaderType
+        {
+            get { return DataReader.GetType().ToString(); }
+        }
+    }
+}
+
